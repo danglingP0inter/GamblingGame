a95b6b181939ac0113ef2e2ae847506a8371c03a
Kirti Verma <kirtiverma603@gmail.com>
2020-07-30 13:26:57 +0530

    Initial Commit

---
+++ SwiftUITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by Kirti Verma on 7/30/20.
+//  Copyright © 2020 Kirti Verma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
