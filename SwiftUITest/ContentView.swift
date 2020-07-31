491c8cc91888000d0777f98e16f58acaaecd59a0
Kirti Verma <kirtiverma603@gmail.com>
2020-07-31 20:17:50 +0530

    yippiegit status! it's been done

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -8,12 +8,38 @@
 
 import SwiftUI
 
+//Play this and you'll know why one shouldn't gamble :)
 struct ContentView: View {
+    
+    @State private var credits = 1000
+    @State private var index1 = 0
+    @State private var index2 = 1
+    @State private var index3 = 2
+    
+    private var images: [Image] = [Image("apple"),
+                                   Image("donut"),
+                                   Image("lemon")]
+    
+    func onSpin() {
+        index1 = Int.random(in: 0..<3)
+        index2 = Int.random(in: 0..<3)
+        index3 = Int.random(in: 0..<3)
+        
+        if (index1 == index2 && index2 == index3) {
+            credits += 100
+        } else {
+            credits -= 50
+        }
+    }
+    
     var body: some View {
         ZStack {
+            // background
             Rectangle()
                 .foregroundColor(Color.init(red: 200/255, green: 143/255, blue: 32/255))
                 .edgesIgnoringSafeArea(.all)
+            
+            //tilted card over background
             Rectangle()
                 .foregroundColor(Color.init(red: 228/255, green: 195/255, blue: 76/255))
                 .rotationEffect(Angle.init(degrees: 45))
@@ -23,8 +49,9 @@ struct ContentView: View {
                 
                 Spacer()
                 
+                //heading
                 HStack {
-                    Image(systemName: "star.fill")
+                    Image(systemName: "star.fill")    //SFSymbols: They're cool !!!
                         .foregroundColor(.yellow)
                     Text("SwiftUI Slots")
                         .bold()
@@ -35,7 +62,8 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                Text("Credits: 1000")
+                //Credits
+                Text("Credits: \(credits)")
                     .padding(10)
                     .background(Color.white.opacity(0.5))
                     .cornerRadius(20)
@@ -43,19 +71,19 @@ struct ContentView: View {
                 Spacer()
 
                 HStack {
-                    Image("apple")
+                    images[index1]
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .background(Color.white.opacity(0.5))
                         .cornerRadius(20)
                     
-                    Image("donut")
+                    images[index2]
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .background(Color.white.opacity(0.5))
                         .cornerRadius(20)
                     
-                    Image("lemon")
+                    images[index3]
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .background(Color.white.opacity(0.5))
@@ -65,12 +93,12 @@ struct ContentView: View {
                 Spacer()
                 
                 Button(action: {
-                    // to do
+                    self.onSpin()
                 }, label: {
                     Text("Spin")
                         .bold()
-                        .padding([.leading, .trailing], 10)
-                        .padding()
+                        .padding([.leading, .trailing], 30)
+                        .padding(.all, 10)
                         .background(Color.pink)
                         .foregroundColor(Color.white)
                         .cornerRadius(20)
@@ -78,8 +106,6 @@ struct ContentView: View {
                 
                 Spacer()
             }
-                
-            
         }
     }
 }
