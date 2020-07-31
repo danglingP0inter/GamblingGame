f0b749209dc1068d53041af434a7be38e92f7a00
Kirti Verma <kirtiverma603@gmail.com>
2020-07-31 15:06:03 +0530

    basic UI completed

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -10,7 +10,77 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color.init(red: 200/255, green: 143/255, blue: 32/255))
+                .edgesIgnoringSafeArea(.all)
+            Rectangle()
+                .foregroundColor(Color.init(red: 228/255, green: 195/255, blue: 76/255))
+                .rotationEffect(Angle.init(degrees: 45))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                Spacer()
+                
+                HStack {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    Text("SwiftUI Slots")
+                        .bold()
+                        .foregroundColor(.white)
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }.scaleEffect(2)
+                
+                Spacer()
+                
+                Text("Credits: 1000")
+                    .padding(10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                
+                Spacer()
+
+                HStack {
+                    Image("apple")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    
+                    Image("donut")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                    
+                    Image("lemon")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .background(Color.white.opacity(0.5))
+                        .cornerRadius(20)
+                }.padding(10)       // alternatively a spacer would also work
+                
+                Spacer()
+                
+                Button(action: {
+                    // to do
+                }, label: {
+                    Text("Spin")
+                        .bold()
+                        .padding([.leading, .trailing], 10)
+                        .padding()
+                        .background(Color.pink)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(20)
+                })
+                
+                Spacer()
+            }
+                
+            
+        }
     }
 }
 
